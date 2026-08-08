3a436690cd37af982879b9eba9992d20f9e19821
Jaden Law <jadenlaw4@gmail.com>
2026-08-08 16:32:07 -0400

    Added drawing screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import DrawingScreen from "./components/DrawingScreen";
 
 export default function App() {
   return (
     <main className="starting-page">
       <h1>ScribblePark</h1>
       <p>Shared project foundation is running.</p>
+      <DrawingScreen></DrawingScreen>
     </main>
   );
 }
